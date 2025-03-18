49a260eef41eb650f4b9f09db28e2851c823f379
Tamás Tóth <tomjtoth@gmail.com>
2025-03-18 17:02:01 +0200

    fixed indents in 2 tests

--- placeholder.test.ts
+++ placeholder.test.ts
@@ -10,12 +10,12 @@ test("placeholder", () => {
 describe("imported text", () => {
   test("has correct name assigned to instance of Conway", () => {
     const patt = new Conway(`
-    #N Gosper glider gun
-    #C This was the first gun discovered.
-    #C As its name suggests, it was discovered by Bill Gosper.
-    x = 36, y = 9, rule = B3/S23
-    24bo$22bobo$12b2o6b2o12b2o$11bo3bo4b2o12b2o$2o8bo5bo3b2o$2o8bo3bob2o4b
-    obo$10bo5bo7bo$11bo3bo$12b2o!
+      #N Gosper glider gun
+      #C This was the first gun discovered.
+      #C As its name suggests, it was discovered by Bill Gosper.
+      x = 36, y = 9, rule = B3/S23
+      24bo$22bobo$12b2o6b2o12b2o$11bo3bo4b2o12b2o$2o8bo5bo3b2o$2o8bo3bob2o4b
+      obo$10bo5bo7bo$11bo3bo$12b2o!
     `);
 
     expect(patt.name).to.equal("Gosper glider gun");
@@ -23,13 +23,13 @@ describe("imported text", () => {
 
   test("x,y,rule can be retrieved from input", () => {
     const patt = new Conway(`
-        #N Gosper glider gun
-        #C This was the first gun discovered.
-        #C As its name suggests, it was discovered by Bill Gosper.
-        x = 36, y = 9, rule = B3/S23
-        24bo$22bobo$12b2o6b2o12b2o$11bo3bo4b2o12b2o$2o8bo5bo3b2o$2o8bo3bob2o4b
-        obo$10bo5bo7bo$11bo3bo$12b2o!
-        `);
+      #N Gosper glider gun
+      #C This was the first gun discovered.
+      #C As its name suggests, it was discovered by Bill Gosper.
+      x = 36, y = 9, rule = B3/S23
+      24bo$22bobo$12b2o6b2o12b2o$11bo3bo4b2o12b2o$2o8bo5bo3b2o$2o8bo3bob2o4b
+      obo$10bo5bo7bo$11bo3bo$12b2o!
+    `);
 
     expect(patt.x).to.equal(36);
     expect(patt.y).to.equal(9);
