32f2d3989fdefba43c098c4d98f33c7514300e4d
Tamás Tóth <tomjtoth@gmail.com>
2025-03-19 12:22:05 +0200

    WiP: lunchtime

--- src/conway.ts
+++ src/conway.ts
@@ -1,24 +1,68 @@
+const ASCII_DOT = "·";
 const RE_NAME = /^\s*#N (.+)/m;
 const RE_XY_RULE =
   /^\s*x\s*=\s*(\d+)\s*,\s*y\s*=\s*(\d+)\s*(?:,\s*rule\s*=\s*(.+))?/m;
 
+const RE_DATA = /(\d*)([bo$!])/g;
+
 export class Conway {
   name: string;
-  x: number;
-  y: number;
   gen: number;
   rule: string;
+  #state: boolean[][] = [];
 
   constructor(str: string, gen: number = 1) {
+    this.gen = gen;
+
     const nameMatch = str.match(RE_NAME);
     this.name = nameMatch ? nameMatch[1] : "anonymous";
-    const [, x, y, rule] = str.match(RE_XY_RULE)!;
+    const xyrMatch = str.match(RE_XY_RULE)!;
+    this.rule = xyrMatch[3];
 
-    this.x = Number(x);
-    this.y = Number(y);
-    this.gen = gen;
-    this.rule = rule;
+    const width = Number(xyrMatch[1]);
+    // const height = Number(xyrMatch[2]);
+    let row: boolean[] = [];
+
+    function finishLine() {
+      while (row.length < width) row.push(false);
+      this.#state.push([...row]);
+    }
+
+    outer: for (const [, strQty, char] of str
+      .slice(xyrMatch.index! + xyrMatch[0].length)
+      .matchAll(RE_DATA)) {
+      let qty = Number(strQty == "" ? "1" : strQty);
+
+      switch (char) {
+        case "b":
+          while (qty-- > 0) row.push(false);
+          break;
+
+        case "o":
+          while (qty-- > 0) row.push(true);
+          break;
+
+        case "$":
+          finishLine();
+          row = [];
+          break;
+
+        case "!":
+          finishLine();
+          break outer;
+      }
+    }
   }
+
+  get x() {
+    return this.#state[0].length;
+  }
+
+  get y() {
+    return this.#state.length;
+  }
+
+  toString() {}
 
   evolve() {
     while (this.gen-- > 0) this.__iter();
