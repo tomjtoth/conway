f53653c8525675eae91b57c9c50b04e19447d700
Tamás Tóth <tomjtoth@gmail.com>
2025-03-20 12:30:18 +0200

    ignoring the web samples' seemingly random linebreak (varying at pos 69/70) in tests, also gens 20+ (that would require expanding state)

--- src/conway.test.ts
+++ src/conway.test.ts
@@ -4,6 +4,7 @@ import { spy } from "sinon";
 
 import { Conway } from "./conway";
 
+const RE_WHITESPACE = /\s+/g;
 export const GOSPER = `
   #N Gosper glider gun
   #C This was the first gun discovered.
@@ -75,15 +76,45 @@ describe("imported text", () => {
     );
   });
 
+  // below data retrieved from: https://conwaylife.com/
   test("after n iterations RLE is as expected", () => {
-    const patt = new Conway(GOSPER, 5);
+    for (const [rle, gen] of [
+      [
+        `22b2o$24bo$11b2o12bo8b2o$11b2o4bo7bo8b2o$2o6b2o5b2o8bo$2o5b3o5bo2b2o4b
+        o$8b2o6b5ob2o$11b2o4bo$11b2o!`,
+        5,
+      ],
+      [
+        `23b2o$23b2o$10bo4bo10b2o6b2o$8bobo4bo10b3o5b2o$2o4b2o7bo10b2o$2o4b2o11b
+      2o2b2o$6b2o8b2o2bo2b2o$8bobo5b4o$10bo7bo!`,
+        10,
+      ],
+      [
+        `27bo$26b4o$9b2o14b2obobo3b2o$bo7bo2bo11b3obo2bo2b2o$o3b2o7bo11b2obobo
+        $o5bo6bo12b4o$b5o7bo7bo5bo$9bo2bo9bo$9b2o9b3o!`,
+        19,
+      ],
+      // [
+      //   `26b2o$25bo3bo$9b2o13bo5bo3b2o$9b2o13bo3bob2o2b2o$2o3bo6b2o10bo5bo$obo
+      // 3bo5b3o10bo3bo$b5o6b2o12b2o$2b3o4b2o9bobo$9b2o10b2o$21bo!`,
+      //   20,
+      //   // this is 1 row more than the original
+      // ],
+      // [
+      //   `26b2o$25bo3bo$9b2o13bo5bo3b2o$9b2o13bo3bob2o2b2o$2o3bo6b2o10bo5bo$obo
+      // 3bo5b3o10bo3bo$b5o6b2o12b2o$2b3o4b2o9bobo$9b2o10b2o$21bo!`,
+      //   50,
+      //   // this is n rows more than the original
+      // ],
+    ]) {
+      const patt = new Conway(GOSPER, gen as number);
 
-    patt.evolve();
+      patt.evolve();
 
-    // retrieved from: https://conwaylife.com/
-    expect(patt.RLE).to.equal(
-      `22b2o$24bo$11b2o12bo8b2o$11b2o4bo7bo8b2o$2o6b2o5b2o8bo$2o5b3o5bo2b2o4b
-      o$8b2o6b5ob2o$11b2o4bo$11b2o!`.replaceAll(" ", "")
-    );
+      const mine = patt.RLE.replaceAll(RE_WHITESPACE, "");
+      const fromWeb = (rle as string).replaceAll(RE_WHITESPACE, "");
+
+      expect(mine).to.equal(fromWeb);
+    }
   });
 });
